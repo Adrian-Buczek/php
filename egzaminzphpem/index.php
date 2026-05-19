00fa2d3e71f18ee8425726a9570ab54edb08891d
Adrian-Buczek <buczekadrian88@gmail.com>
2026-05-19 10:27:16 +0200

    Add files via upload

---
+++ egzaminzphpem/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Temperatura w Lipcu</h1>
+    <table>
+        <tr>
+            <th>Miasto</th>
+            <th>Kraj</th>
+            <th>Temperatura</th>
+            <th>Pogoda</th>
+        </tr>
+       
+    </table>
+   <hr>
+   <h2>Średnie temperatury w roku:</h2>
+   <a href="index.php">Styczeń</a>
+   <a href="index.php">Luty</a>
+   <a href="index.php">Marzec</a>
+   <a href="index.php">Kwiecień</a>
+   <a href="index.php">Maj</a>
+   <a href="index.php">Czerwiec</a>
+   <a href="index.php">Lipiec</a>
+   <a href="index.php">Sierpień</a>
+   <a href="index.php">Wrzesień</a>
+   <a href="index.php">Październik</a>
+   <a href="index.php">Listopad</a>
+   <a href="index.php">Grudzień</a>
+
+   <style>
+    table{
+        height: 400px;
+    }
+    tr{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap:10px;
+    }
+    a{
+        color: black;
+        text-decoration: none;
+        font-size: 30px;
+    }
+    a:hover{
+        background-color: gray;
+        color: white;
+    }
+   </style>
+</body>
+</html>
